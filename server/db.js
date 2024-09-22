e197614af19039c46686e138ff2b7c5cc5dac9f9
shdileep <dileepgalla200056@gmail.com>
2024-09-22 22:05:25 +0530

    refactor(db): add idle client error listener and query helper annotations

--- server/db.js
+++ server/db.js
@@ -9,7 +9,18 @@ const pool = new Pool({
   ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false
 });
 
+// Log unexpected errors on idle pool clients
+pool.on('error', (err) => {
+  console.error('Unexpected error on idle PostgreSQL client:', err);
+});
+
 module.exports = {
+  /**
+   * Execute a parameterized SQL query
+   * @param {string} text - SQL query text
+   * @param {Array} [params] - Query parameters
+   */
   query: (text, params) => pool.query(text, params),
   pool
 };
+
